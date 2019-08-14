669f1cb195cd98df8fe361ba9c40b4d48fbee446
Jun <jwan5998@live.cn>
2019-08-14 08:10:21 -0600

    Assignment 4

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -4,6 +4,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,7 +28,7 @@ public class TimeEntryController {
     public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId) {
         TimeEntry timeEntry = timeEntryRepository.find(timeEntryId);
         if(timeEntry == null)
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(timeEntry);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         else
             return ResponseEntity.status(HttpStatus.OK).body(timeEntry);
     }
